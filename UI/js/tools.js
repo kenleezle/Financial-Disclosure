9a36421732e5b8f60ee66d2901b8866220d1bfb2
James Weston <james@dyn-160-39-10-236.dyn.columbia.edu>
2013-02-02 15:08:38 -0500

    UI First

---
+++ UI/js/tools.js
@@ -0,0 +1,67 @@
+var datasetlist = [];
+var comparelist = [];
+
+var Official = function (set) {
+  var listitem = document.createElement('li');
+  
+  var radiolabel = document.createElement('label');
+  $(radiolabel).addClass('radio');
+
+  var radio = document.createElement('input');
+  $(radio).attr('type','radio');
+  $(radio).attr('name','sets');
+
+  $(radiolabel).append(radio);
+  $(radiolabel).append(set.displayName);
+
+  $(listitem).append(radiolabel)
+
+  $(radio).on('click', function () {
+      //clear visual
+      //show officials portfolio
+      for (var i = 0; i < comparelist.length; i++) {
+        $(comparelist[i].checkbox).removeAttr('checked');
+      }
+    })
+  $('#officiallist').append(listitem);
+}
+
+var Comparison = function (comp) {
+  var listitem = document.createElement('li');
+  
+  var checkboxlabel = document.createElement('label');
+  $(checkboxlabel).addClass('checkbox');
+
+  var checkbox = document.createElement('input');
+  $(checkbox).attr('type','checkbox');
+  $(checkbox).attr('name','sets');
+
+  $(checkboxlabel).append(checkbox);
+  $(checkboxlabel).append(comp.displayName);
+
+  $(listitem).append(checkboxlabel)
+
+  $(checkbox).on('click', function () {
+      //toggle comparison dataset
+      console.log($(checkbox).is(':checked'));
+      console.log(comp.dataSetId);
+    })  
+  $('#comparelist').append(listitem);
+
+  this.checkbox = checkbox;
+}
+
+var officials = 'js/officials.json' //URL to JSON Feed of Data components
+var comparisons = 'js/comparisons.json'
+
+d3.json(officials, function (err,sets) {
+    for (var i in sets.children) {
+      datasetlist.push(new Official(sets.children[i]))
+    }
+  })
+
+d3.json(comparisons, function (err,sets) {
+    for (var i in sets.children) {
+      comparelist.push(new Comparison(sets.children[i]))
+    }
+  });
