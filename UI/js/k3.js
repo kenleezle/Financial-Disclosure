1492bcde20d67b60e222e435233ca28b11bf95d3
Ken Leland III <kenleezle@gmail.com>
2013-02-02 21:20:19 -0500

    changed it over to percentage

--- UI/js/k3.js
+++ UI/js/k3.js
@@ -2,7 +2,7 @@ var sp500_json = 'js/SP500.json';
 sp500 = [];
 d3.json(sp500_json, function (err,spobj) {
 	for (var i in spobj.days) {
-		sp500.push([spobj.days[i].date,spobj.days[i].value]);
+		sp500.push([spobj.days[i].date,spobj.days[i].change]);
 	}
 	drawGraph();
 });
