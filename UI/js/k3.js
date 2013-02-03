a97202cdefaf051fa32bfd849a951f610b91ae72
Ken Leland <kenleezle@gmail.com>
2013-02-02 21:11:56 -0500

    refactoring graph bug fix in sp500

---
+++ UI/js/k3.js
@@ -0,0 +1,8 @@
+var sp500_json = 'js/SP500.json';
+sp500 = [];
+d3.json(sp500_json, function (err,spobj) {
+	for (var i in spobj.days) {
+		sp500.push([spobj.days[i].date,spobj.days[i].value]);
+	}
+	drawGraph();
+});
--- ruby_libs/sp500.rb
+++ ruby_libs/sp500.rb
@@ -5,14 +5,17 @@ output["name"] = "SP500"
 output["days"] = Array.new
 benchmark = 1268.80
 change = last_change = 0
+value = last_value = benchmark
 STDIN.readlines.each { | line |
   line.chomp!
   (date,value) = line.split(",")
   if value.nil? then
   	change = last_change
+	value = last_value
   else
   	change = (value.to_f-benchmark)/benchmark
 	last_change = change
+	last_value = value
   end
   output["days"].push({:date=>date,:change=>change,:value=>value})
 }
